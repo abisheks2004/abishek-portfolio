7fc791676d6a66a039fc08ae16fa8d9ebd542864
abisheks2004 <abisheka067@gmail.com>
2025-06-27 16:11:07 +0530

    Initial commit with project files

--- src/components/LeftCharacter.jsx
+++ src/components/LeftCharacter.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Lottie from 'lottie-react';
-import devCharacter from '../assets/dev-character.json';
 
 export default function LeftCharacter() {
+  const [animationData, setAnimationData] = useState(null);
+
+  useEffect(() => {
+    fetch('/animations/dev-character.json')
+      .then((res) => res.json())
+      .then(setAnimationData)
+      .catch(console.error);
+  }, []);
+
+  if (!animationData) return null; // or a loader
+
   return (
     <div className="absolute bottom-[60px] sm:bottom-[80px] md:bottom-[100px] lg:bottom-[20px] left-2 sm:left-6 md:left-16 lg:left-24 z-10 w-[140px] sm:w-[180px] md:w-[260px] lg:w-[300px] drop-shadow-xl">
-      <Lottie animationData={devCharacter} loop autoplay />
+      <Lottie animationData={animationData} loop autoplay />
     </div>
   );
 }
--- src/components/RightCharacter.jsx
+++ src/components/RightCharacter.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Lottie from 'lottie-react';
-import viewerAnimation from '../assets/view-character.json';
 
 export default function RightCharacter() {
+  const [animationData, setAnimationData] = useState(null);
+
+  useEffect(() => {
+    fetch('/animations/view-character.json')
+      .then((res) => res.json())
+      .then(setAnimationData)
+      .catch(console.error);
+  }, []);
+
+  if (!animationData) return null; // Or a loading spinner
+
   return (
     <div className="absolute bottom-[60px] sm:bottom-[80px] md:bottom-[100px] lg:bottom-[80px] right-0 sm:right-4 md:right-16 lg:right-24 z-10 w-[180px] sm:w-[220px] md:w-[260px] lg:w-[320px] flex flex-col items-center">
-      <Lottie animationData={viewerAnimation} loop autoplay />
+      <Lottie animationData={animationData} loop autoplay />
     </div>
   );
 }
