0a47e5cf24cbea0d910903828f80a450a603e620
ABISHEK <164663382+abisheks2004@users.noreply.github.com>
2026-09-03 18:48:32 +0530

    fix: close ChatBubbleLeft component

--- src/components/ChatBubbleLeft.jsx
+++ src/components/ChatBubbleLeft.jsx
@@ -4,13 +4,10 @@ export default function ChatBubbleLeft({ message }) {
       <div className="bg-gradient-to-br from-white to-gray-200 text-black px-3 py-2 sm:px-4 sm:py-3 rounded-2xl shadow-xl relative min-h-[40px] min-w-[100px] transition-all duration-300 break-words">
         <span>{message || "..."}</span>
 
-
         <div
           className="absolute -left-2 bottom-2 w-0 h-0 border-t-[8px] border-t-transparent border-r-[12px] border-r-white border-b-[1px] border-b-transparent"
         />
-
-
-
       </div>
     </div>
   );
+}
