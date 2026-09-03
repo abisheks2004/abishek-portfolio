1ab124250b66244711fd76ddd987e3d3be1ba61a
ABISHEK <164663382+abisheks2004@users.noreply.github.com>
2026-09-03 16:39:03 +0530

    chore: remove unused React import

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import LeftCharacter from './LeftCharacter';
-import RightCharacter from './RightCharacter';
+import LeftCharacter from "./LeftCharacter";
+import RightCharacter from "./RightCharacter";
 
 export default function Character() {
   return (
